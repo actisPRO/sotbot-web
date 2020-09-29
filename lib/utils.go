f3dda50d98705516dc0ac8ccee297348a773c9a3
Denis Karev <dkarev@protonmail.com>
2020-09-30 00:15:08 +0200

    fixed previous commit

--- lib/utils.go
+++ lib/utils.go
@@ -8,15 +8,15 @@ import (
 )
 
 func GetIP(r *http.Request) (string, error) {
-	//Get IP from CF-CONNECTING-IP (because we are using Cloudflare)
-	ip := r.Header.Get("CF-CONNECTING-IP")
+	//Get IP from CF-Connecting-IP (because we are using Cloudflare)
+	ip := r.Header.Get("CF-Connecting-IP")
 	netIP := net.ParseIP(ip)
 	if netIP != nil {
 		return ip, nil
 	}
 
 	//Get IP from X-FORWARDED-FOR header
-	ips := r.Header.Get("X-FORWARDED-FOR")
+	ips := r.Header.Get("X-Forwarded-For")
 	splitIps := strings.Split(ips, ",")
 	for _, ip := range splitIps {
 		netIP := net.ParseIP(ip)
